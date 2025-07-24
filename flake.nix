53d68800bdea925ddf5bf7b1d4daa5c3d693900e
only1thor <user@example.com>
2025-07-24 13:09:53 +0200

    create mini-iso

--- flake.nix
+++ flake.nix
@@ -6,18 +6,26 @@
   };
 
   outputs = { self, nixpkgs, disko, ... }:
-    let
-      system = "x86_64-linux";
-    in {
-      nixosConfigurations = {
-        test = nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./podman_apps_server/configuration.nix
-            disko.nixosModules.disko
-            ./podman_apps_server/disko-config.nix
-          ];
-        };
+  let
+    system = "x86_64-linux";
+  in {
+    nixosConfigurations = {
+      test = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./podman_apps_server/configuration.nix
+          disko.nixosModules.disko
+          ./podman_apps_server/disko-config.nix
+        ];
+      };
+      isomini = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration-live-mini.nix
+          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix"
+        ];
       };
     };
+    packages.x86_64-linux.default = self.nixosConfigurations.isomini.config.system.build.isoImage;
+  };
 }
--- readme.md
+++ readme.md
@@ -1,8 +1,13 @@
-# quick setup/disaster recovery
+# Quick setup/disaster recovery
 
 1. Boot from the recovery iso with preloaded ssh keys.
 2. Then from the root of this repo run: `nixos-anywhere --flake .#test root@192.168.122.141`
 
-## updating docker-compose.yaml/nix
+## Updating docker-compose.yaml/nix
 to generate docker-compose.nix use:
 `nix run github:aksiksi/compose2nix -- -project=testpods`
+
+## Building boot[strap] iso
+build minimal iso with preloaded ssh keys:
+
+`nix build .#nixosConfigurations.isomini.config.system.build.isoImage`
